dc3acbea7232ef19268edfd831c7fa41be20912e
alpheus masanga <alklyn@gmail.com>
2017-07-23 02:57:43 +0200

    Feat: Add filter funtion

--- js/app.js
+++ js/app.js
@@ -8,9 +8,28 @@ var Location = function (data) {
 
 var viewModel = function () {
   var self = this;
+  self.searchValue = ko.observable('');
 
   self.locations = ko.observableArray([]);
   LOCATIONS.forEach(function (place) {
-    self.locations.push(new Location(place))
+    self.locations.push(new Location(place));
   });
+
+  self.updateLocationsList = function () {
+    // Build a list of locations to display in the page.
+
+    self.locations([]);
+    LOCATIONS.forEach(function (place) {
+      var addItem = true;
+      if ((place.title.toLowerCase().indexOf(self.searchValue().toLowerCase()) > -1) || (self.searchValue().length === 0)) {
+        self.locations.push(new Location(place));
+      }
+    });
+  }
+
+
+  self.searchValue.subscribe(function () {
+    self.updateLocationsList();
+  });
+  self.updateLocationsList();
 }
