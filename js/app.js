6cc913d542ca18fd9475433ef1c217c26ab82485
alpheus masanga <alklyn@gmail.com>
2017-07-24 02:10:00 +0200

    Feat: Add div for displaying wikipedia articles about location

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
+var getWikipediaData = function (placeName) {
+  // load wikipedia data
+  var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + placeName + '&format=json&callback=wikiCallback';
+  var wikiRequestTimeout = setTimeout(function(){
+      $wikiElem.text("failed to get wikipedia resources");
+  }, 8000);
+
+  $.ajax({
+      url: wikiUrl,
+      dataType: "jsonp",
+      jsonp: "callback",
+      success: function( response ) {
+          var articleList = response[1];
+
+          for (var i = 0; i < articleList.length; i++) {
+              articleStr = articleList[i];
+              var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+              $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+          };
+
+          clearTimeout(wikiRequestTimeout);
+      }
+  });
+}
+
 var Location = function (data) {
   var self = this;
 
@@ -29,6 +54,9 @@ var viewModel = function () {
     });
   }
 
+  self.getLocationData = function (name) {
+    console.log('Getting data about: ' + name);
+  }
 
   self.searchValue.subscribe(function () {
     self.updateLocationsList();
