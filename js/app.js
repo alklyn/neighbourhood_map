ee24f2a63d70e91fd2053fad0453c725dc884c24
alpheus masanga <alklyn@gmail.com>
2017-07-29 15:02:54 +0200

    Fix: Replace non-responsive location list with responsive one

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ var attractions = [
     {title: "Camps Bay Beach", state: "Cape Town", country: "South Africa", coordinates: {lat: -33.9506605, lng: 18.3776413}},
     {title: "Constantia", state: "Cape Town", country: "South Africa", coordinates: {lat: -34.0276319, lng: 18.396417}},
     {title: "Hottentots Holland Nature Reserve", state: "Cape Town", country: "South Africa", coordinates: {lat: -34.0334007, lng: 19.0632778}},
-    {title: "GrandWest Casino and Entertainment World", state: "Cape Town", country: "South Africa", coordinates: {lat: -33.9177148, lng: 18.547582}},
+    {title: "Grand West Casino and Entertainment World", state: "Cape Town", country: "South Africa", coordinates: {lat: -33.9177148, lng: 18.547582}},
     {title: "Kirstenbosch National Botanical Garden", state: "Cape Town", country: "South Africa", coordinates: {lat: -33.9875011, lng: 18.432722}},
     {title: "Platteklip Gorge", state: "Cape Town", country: "South Africa", coordinates: {lat: -33.9613302, lng: 18.4087109}},
     {title: "The Cape Wheel", state: "Cape Town", country: "South Africa", coordinates: {lat: -33.9053211, lng: 18.4199256}}
