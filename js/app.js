fbc01ff14ae08f630780a44d34a10b3808fc351e
alpheus masanga <alklyn@gmail.com>
2017-07-30 15:25:18 +0200

    Refactor: Move location data to separate file

--- js/app.js
+++ js/app.js
@@ -1,22 +1,3 @@
-// Popolar attractions in Cape Town.
-var attractions = [
-    {title: "Table mountain", state: "Cape Town", country: "South Africa", coordinates: {lat: -33.962822, lng: 18.4076519}},
-    {title: "Cape of Good Hope", state: "Cape Town", country: "South Africa", coordinates: {lat: -34.3568425, lng: 18.4739882}},
-    {title: "Two Oceans Aquarium", state: "Cape Town", country: "South Africa", coordinates: {lat: -33.9080317, lng: 18.4176607}},
-    {title: "Victoria and Alfred Waterfront", state: "Cape Town", country: "South Africa", coordinates: {lat: -33.90363, lng: 18.420529}},
-    {title: "Boulders Beach", state: "Cape Town", country: "South Africa", coordinates: {lat: -34.192783, lng: 18.4327457}},
-    {title: "Robben Island", state: "Cape Town", country: "South Africa", coordinates: {lat: -33.8076073, lng: 18.3712309}},
-    {title: "World of Birds Wildlife Sanctuary and Monkey Park", state: "Cape Town", country: "South Africa", coordinates: {lat: -34.016799, lng: 18.36196}},
-    {title: "Camps Bay Beach", state: "Cape Town", country: "South Africa", coordinates: {lat: -33.9506605, lng: 18.3776413}},
-    {title: "Constantia", state: "Cape Town", country: "South Africa", coordinates: {lat: -34.0276319, lng: 18.396417}},
-    {title: "Hottentots Holland Nature Reserve", state: "Cape Town", country: "South Africa", coordinates: {lat: -34.0334007, lng: 19.0632778}},
-    {title: "Grand West Casino and Entertainment World", state: "Cape Town", country: "South Africa", coordinates: {lat: -33.9177148, lng: 18.547582}},
-    {title: "Kirstenbosch National Botanical Garden", state: "Cape Town", country: "South Africa", coordinates: {lat: -33.9875011, lng: 18.432722}},
-    {title: "Platteklip Gorge", state: "Cape Town", country: "South Africa", coordinates: {lat: -33.9613302, lng: 18.4087109}},
-    {title: "The Cape Wheel", state: "Cape Town", country: "South Africa", coordinates: {lat: -33.9053211, lng: 18.4199256}}
-];
-
-
 // Sort by title. From mozilla example.
 attractions.sort(function (a, b) {
     "use strict";
