728b8918f07c101380c543c2f30a3e23b2339e03
alpheus masanga <alklyn@gmail.com>
2017-07-31 00:51:42 +0200

    Feat: Add error handling method for loading google maps

--- js/map.js
+++ js/map.js
@@ -214,3 +214,12 @@ var addContent = function functionName(marker, infowindow) {
         }
     });
 };
+
+
+// Handles errors
+var googleError = function () {
+    console.log("Oh snap!");
+    var errorMsg = "<div class=\"alert alert-danger\" role=\"alert\">Error!. Failed to load Google maps!</div>";
+    var mapElem = document.getElementById("map");
+    mapElem.innerHTML = errorMsg;
+};
