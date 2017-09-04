603c5304617a30086e58404447d0d682f01b4111
alpheus masanga <alklyn@gmail.com>
2017-09-05 00:11:28 +0200

    Refactor: Switch to ES6 class definations

--- js/app.js
+++ js/app.js
@@ -1,101 +1,118 @@
 // Sort by title. From mozilla example.
-attractions.sort(function (a, b) {
+attractions.sort((a, b) => {
     "use strict";
-    let titleA = a.title.toLowerCase(); // ignore upper and lowercase
-    let titleB = b.title.toLowerCase(); // ignore upper and lowercase
+    const titleA = a.title.toLowerCase(); // ignore upper and lowercase
+    const titleB = b.title.toLowerCase(); // ignore upper and lowercase
     if (titleA < titleB) {
         return -1;
     }
     if (titleA > titleB) {
         return 1;
     }
-
     return 0;
 });
 
 
-let Location = function (data) {
-    "use strict";
-    let self = this;
-    self.title = data.title;
-    self.state = data.state;
-    self.country = data.country;
-    self.coordinates = data.coordinates;
-};
+class Location {
+    constructor (data) {
+        "use strict";
+        const self = this;
+        self.title = data.title;
+        self.state = data.state;
+        self.country = data.country;
+        self.coordinates = data.coordinates;
+    }
+}
 
-
-let ViewModel = function () {
-    "use strict";
-    let self = this;
-    self.searchValue = ko.observable("");
-    self.wikiError = ko.observable(false);
-    self.wikiData = ko.observableArray([]);
-    self.wikiNoResultsFound = ko.observable(false);
-    self.wikiSearchMade = ko.observable(false);
-    self.unwantedPlaces = [];
-    self.googleError = ko.observable(false);
-
-    self.locations = ko.observableArray([]);
-    attractions.forEach(function (place) {
-        self.locations.push(new Location(place));
-    });
-
-    // Build a list of locations to display in the page.
-    self.updateLocationsList = function () {
-        self.locations([]);
+class ViewModel {
+    constructor() {
+        const self = this;
+        self.searchValue = ko.observable("");
+        self.wikiError = ko.observable(false);
+        self.wikiData = ko.observableArray([]);
+        self.wikiNoResultsFound = ko.observable(false);
+        self.wikiSearchMade = ko.observable(false);
         self.unwantedPlaces = [];
-        attractions.forEach(function (place) {
-            if ((place.title.toLowerCase().indexOf(self.searchValue().toLowerCase()) > -1) || (self.searchValue().length === 0)) {
-                self.locations.push(new Location(place));
-            } else {
-                // save a list of names of unwanted locations
-                self.unwantedPlaces.push(place.title);
-            }
+        self.googleError = ko.observable(false);
+        self.locations = ko.observableArray([]);
+
+        attractions.forEach((place) => {
+            self.locations.push(new Location(place));
         });
-    };
+
+        self.searchValue.subscribe(() => {
+            self.updateLocationsList();
+            displayMarkers(markers, self.unwantedPlaces);
+        });
+
+
+        // Build a list of locations to display in the page.
+        self.updateLocationsList = () => {
+            self.locations([]);
+            self.unwantedPlaces = [];
+            attractions.forEach(function (place) {
+                if ((place.title.toLowerCase().indexOf(self.searchValue().toLowerCase()) > -1) || (self.searchValue().length === 0)) {
+                    self.locations.push(new Location(place));
+                } else {
+                    // save a list of names of unwanted locations
+                    self.unwantedPlaces.push(place.title);
+                }
+            });
+        };
+
+        // Search for wikipedia articles about the place.
+        self.getWikipediaData = (placeName) => {
+            // Reset array before loading new data
+            self.wikiData([]);
+            self.wikiError(false);
+            let query = self.formatQuery(placeName);
+            console.log(query);
+
+            // load wikipedia data
+            let wikiUrl = `http://en.wikipedia.org/w/api.php?action=opensearch&search=${query}&format=json&callback=wikiCallback`;
+            $.ajax({
+                // ajax settings
+                url: wikiUrl,
+                dataType: "jsonp",
+                jsonp: "callback"
+            }).done(function (response) {
+                // successful
+                // Check if wikipedia search returned any results
+                if (self.wikiSearchMade()) {
+                    self.wikiNoResultsFound(response[1].length === 0);
+                }
+                self.wikiData(self.formatWikiData(response));
+            }).fail(function (jqXHR, textStatus) {
+                // error handling
+                self.wikiError(true);
+            });
+        };
+
+        // Get information about a place
+        self.getLocationData = (place) => {
+            // console.log(place);
+            this.wikiSearchMade(true);
+            this.getWikipediaData(place.title);
+            animateMarker(place.title);
+        };
+
+        self.updateLocationsList();
+    }
 
     // Format search terms. Replace spaces with plus signs
-    self.formatQuery = function (query) {
+    formatQuery(query) {
         query = query.trim();
         let result = "";
         for(let i = 0; i < query.length; i++) {
             result += query[i].replace(" ","+");
         }
         return result;
-    };
-
-    // Search for wikipedia articles about the place.
-    self.getWikipediaData = function (placeName) {
-        // Reset array before loading new data
-        self.wikiData([]);
-        self.wikiError(false);
-        let query = self.formatQuery(placeName);
-        console.log(query);
-
-        // load wikipedia data
-        let wikiUrl = `http://en.wikipedia.org/w/api.php?action=opensearch&search=${query}&format=json&callback=wikiCallback`;
-        $.ajax({
-            // ajax settings
-            url: wikiUrl,
-            dataType: "jsonp",
-            jsonp: "callback"
-        }).done(function (response) {
-            // successful
-            // Check if wikipedia search returned any results
-            if (self.wikiSearchMade()) {
-                self.wikiNoResultsFound(response[1].length === 0);
-            }
-            self.wikiData(self.formatWikiData(response));
-        }).fail(function (jqXHR, textStatus) {
-            // error handling
-            self.wikiError(true);
-        });
-    };
+    }
 
     // Reformat Wikipedia response to make it easier to use
     // Wikipedia response is split into 3 main sections
     // Title, snippet & url
-    self.formatWikiData = function (response) {
+    formatWikiData(response) {
         let len = response[1].length;
         let data = [];
         for (let m = 0; m < len; m++) {
@@ -128,32 +145,16 @@ let ViewModel = function () {
         }
         // console.log(data);
         return data;
-    };
-
-
-    // Get information about a place
-    self.getLocationData = function (place) {
-        // console.log(place);
-        self.wikiSearchMade(true);
-        self.getWikipediaData(place.title);
-        animateMarker(place.title);
-    };
-
-
-    self.searchValue.subscribe(function () {
-        self.updateLocationsList();
-        displayMarkers(markers, self.unwantedPlaces);
-    });
+    }
 
     // Display error message if if there is an error loading map.
-    self.setGoogleError = function (status) {
-        self.googleError(status);
-    };
-    self.updateLocationsList();
-};
+    setGoogleError(status) {
+        this.googleError(status);
+    }
+}
 
 
-let viewModel = new ViewModel();
+const viewModel = new ViewModel();
 
 // Delay the initialization of ViewModel until the page has loaded.
 // Solves the error I was getting below
